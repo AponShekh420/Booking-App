b9c6274da0558b5fafbbb69eadf46f79c2ad9dbf
Apon Shekh <aponshekh420@gmail.com>
2025-01-15 18:19:20 +0600

    The header has done for searchandcategory page

---
+++ src/components/common/header/CategorySlide.tsx
@@ -0,0 +1,94 @@
+"use client"
+import React, { useRef, useState } from 'react';
+import { Virtual, Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import categoriesList from '@/data/categorieslist';
+import { Swiper as SwiperType } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import Image from "next/image";
+import CategoryItem from './CategoryItem';
+
+// interface CategorySlideProps {
+//   slideTo: (index: number) => void
+// }
+
+
+const CategorySlide = () => {
+  const [active, setActive] = useState<string>("Trending");
+    
+  return (
+    <div className="w-full mx-auto categoriesSlideHeader categoriesSlide">
+      <div className="flex items-start h-auto">
+        <div className="z-40 md:flex hidden items-center">
+          <button className="custom-prev-category w-[35.42px] h-[35.42px] border-[0.92px] border-[#9B9B9B] rounded-full flex items-center justify-center swiper-button-disabled">
+            <Image
+              src={"/assets/icons/categories/Arrows/left-arrow.svg"}
+              alt="left arrow"
+              width={8.13}
+              height={13.71}
+            />
+          </button>
+        </div>
+
+        <Swiper
+          modules={[Navigation]}
+          slidesPerView="auto" // Dynamic width calculation
+          spaceBetween={48} // Gap between slides
+          centeredSlides={false} // Prevent centering when all slides fit
+          centerInsufficientSlides={true} // Center slides if they are fewer than the available space
+          navigation={{
+            nextEl: ".custom-next-category",
+            prevEl: ".custom-prev-category",
+          }}
+          onReachBeginning={() => {
+            document?.querySelector(".custom-prev-category")?.classList.add("swiper-button-disabled");
+          }}
+          onReachEnd={() => {
+            document?.querySelector(".custom-next-category")?.classList.add("swiper-button-disabled");
+          }}
+          onFromEdge={() => {
+            document?.querySelector(".custom-prev-category")?.classList.remove("swiper-button-disabled");
+            document?.querySelector(".custom-next-category")?.classList.remove("swiper-button-disabled");
+          }}
+          breakpoints={{
+            0: { spaceBetween: 24 },
+            240: { spaceBetween: 32 },
+            340: { spaceBetween: 34 },
+            640: { spaceBetween: 35 },
+            768: { spaceBetween: 38 },
+            1024: { spaceBetween: 43 },
+            1280: { spaceBetween: 48 },
+          }}
+        >
+          {categoriesList.map((category, index) => (
+            <SwiperSlide
+              key={index}
+              className="!w-auto" // Allows dynamic width calculation
+            >
+              <CategoryItem category={category} active={active} setActive={setActive} index={index}/>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        {/* Custom Navigation Buttons */}
+        <div className="z-40 md:block hidden">
+          <button className="custom-next-category w-[35.42px] h-[35.42px] border-[0.92px] border-[#9B9B9B] rounded-full flex items-center justify-center">
+            <Image
+              src={"/assets/icons/categories/Arrows/right-arrow.svg"}
+              alt="right arrow"
+              width={8.13}
+              height={13.71}
+            />
+          </button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default CategorySlide;
---
+++ src/components/common/header/Searchbar/StayDatesRangeInput.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import React, { Fragment, useState, FC } from 'react'
+import {
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+  Transition,
+} from '@headlessui/react'
+import { CalendarIcon } from '@heroicons/react/24/outline'
+import DatePicker from 'react-datepicker'
+import ClearDataButton from './ClearDataButton'
+import DatePickerCustomHeaderTwoMonth from '@/components/DatePickerCustomHeaderTwoMonth'
+import DatePickerCustomDay from '@/components/DatePickerCustomDay'
+import Image from 'next/image'
+
+export interface StayDatesRangeInputProps {
+  className?: string
+  fieldClassName?: string
+}
+
+const StayDatesRangeInput: FC<StayDatesRangeInputProps> = ({
+  className = 'lg:nc-flex-2 ',
+  fieldClassName = 'nc-hero-field-padding',
+}) => {
+  const [startDate, setStartDate] = useState<Date | null>(
+    new Date('2023/02/06'),
+  )
+  const [endDate, setEndDate] = useState<Date | null>(new Date('2023/02/23'))
+
+  const onChangeDate = (dates: [Date | null, Date | null]) => {
+    const [start, end] = dates
+    setStartDate(start)
+    setEndDate(end)
+  }
+
+  const renderInput = () => (
+    <div className="lg:px-[16px] px-[7.8px] lg:h-[37px] h-[20.62px] border-r-[1px] flex items-center border-[#D8D8D8]">
+			<Image 
+				src={"assets/icons/searchbar/Vector.svg"} 
+				alt="AI" 
+				width={100} 
+				height={100}
+				className="lg:w-[30px] lg:h-[30px] h-[16.72px] w-[16.72px] sm:h-[27.72px] sm:w-[27.72px]"
+			/>
+		</div>
+  )
+
+	const renderButtonList = () => (
+		Array.from({ length: 6 }).map((_, index) => (
+			<button
+				key={index}
+				className="lg:w-[151px] w-5/12 h-[29px] rounded-[44px] bg-[#F3F3F3] hover:bg-[#E9082A] text-[13px] leading-[33.63px] font-[700] text-black hover:text-white"
+			>
+				1{index + 1}: 00 AM
+			</button>
+		))
+  )
+
+  return (
+		<div className='flex md:gap-x-[30px] lg:gap-x-[40px] justify-between w-full flex-col lg:flex-row overflow-y-auto h-[376px]'>
+			<div className="w-full">
+				<DatePicker
+					selected={startDate}
+					onChange={onChangeDate}
+					startDate={startDate}
+					endDate={endDate}
+					selectsRange
+					monthsShown={1}
+					showPopperArrow={false}
+					inline
+					className='w-full'
+					renderCustomHeader={(p) => (
+						<DatePickerCustomHeaderTwoMonth {...p} />
+					)}
+					renderDayContents={(day, date) => (
+						<DatePickerCustomDay dayOfMonth={day} date={date} />
+					)}
+				/>
+			</div>
+			<div className="w-full md:w-auto">
+				<p className='font-[700] text-[17.12px] leading-[33.63px] text-center mb-[2px]'>Time</p>
+				<div className='flex lg:flex-col gap-y-[8px] lg:gap-x-[0px] gap-x-[8px] flex-wrap justify-center'>
+					{renderButtonList()}
+				</div>
+			</div>
+		</div>
+  )
+}
+
+export default StayDatesRangeInput
