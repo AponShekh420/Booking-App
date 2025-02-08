50785ed91d034e4415f32a7e8d5de690cbb89ba3
Apon Shekh <aponshekh420@gmail.com>
2025-02-08 12:52:12 +0600

    modified the article page and article two page

--- src/components/article/latest-topic/LatestTopicSection.tsx
+++ src/components/article/latest-topic/LatestTopicSection.tsx
@@ -29,10 +29,22 @@ const topics = [
 		desc: 'Learn why men are increasingly choosing treatments like Botox, laser hair removal, and more to enhance their appearance.',
 	},
 	{
-		id: 3,
+		id: 4,
 		image: topic3,
 		title: 'From Botox to Laser Hair Removal',
 		desc: 'Learn why men are increasingly choosing treatments like Botox, laser hair removal, and more to enhance their appearance.',
+	},
+	{
+		id: 5,
+		image: topic1,
+		title: 'Do hair loss treatments work?',
+		desc: 'Curious about hair loss treatments? Explore the science behind the most effective solutions, from medications to advanced therapies...',
+	},
+	{
+		id: 6,
+		image: topic3,
+		title: 'Do hair loss treatments work?',
+		desc: 'Curious about hair loss treatments? Explore the science behind the most effective solutions, from medications to advanced therapies...',
 	},
 ]
 export default function LatestTopicSection() {
@@ -42,86 +54,22 @@ export default function LatestTopicSection() {
 				<h5 className="font-heading text-[34.72px] font-[700] uppercase leading-[52.08px] text-custom-red sm:text-[42.72px] sm:leading-[62.08px] md:text-[50.72px] md:leading-[72.08px] lg:text-[58.72px] lg:leading-[82.08px] xl:text-[64.288px] xl:leading-[80.43px]">
 					LATEST TOPICS:
 				</h5>
-				<div className="w-full">
-					<Swiper
-						modules={[Navigation]}
-						// onSwiper={setSwiperRef}
-						slidesPerView={3}
-						slidesPerGroup={3}
-						centeredSlides={false}
-						spaceBetween={17}
-						// virtual
-						navigation={{
-							nextEl: '.custom-next-topic',
-							prevEl: '.custom-prev-topic',
-						}}
-						loopAddBlankSlides={true}
-						breakpoints={{
-							768: {
-								slidesPerView: 3,
-								slidesPerGroup: 3,
-							},
-							240: {
-								slidesPerView: 2,
-								slidesPerGroup: 4,
-							},
-						}}
-					>
+				<div className="w-full grid-cols-2 grid gap-x-2 lg:grid-cols-3 sm:gap-y-20 sm:gap-x-4 gap-y-14 xs:gap-x-3">
+					
 						{topics.map(({ id, title, image, desc }, index) => (
-							<div
-								className={`nc-SectionSliderNewCategories grid grid-rows-2`}
-								key={index}
-							>
-								<div className="relative flow-root">
-									<div className="hiddenScrollbar relative -mx-2 flex snap-x snap-mandatory overflow-x-auto lg:-mx-3.5">
-										<div
-											className={`min-h-[340px] w-[17rem] shrink-0 snap-start overflow-hidden rounded-[15px] px-2 lg:w-[25%] lg:px-3.5 xl:w-[20%]`}
-										>
-											<SwiperSlide virtualIndex={index}>
-												<TopicCard
-													key={id}
-													title={title}
-													image={image}
-													desc={desc}
-												/>
-												<div className="mt-[80px]">
-													<TopicCard
-														key={id}
-														title={title}
-														image={image}
-														desc={desc}
-													/>
-												</div>
-											</SwiperSlide>
-										</div>
-									</div>
-								</div>
-							</div>
+							<TopicCard
+								key={id}
+								title={title}
+								image={image}
+								desc={desc}
+							/>
 						))}
-
-						{/* add button and custom arrow for slide*/}
-						<div className="mt-[58.53px] flex w-full items-center justify-center gap-[14.7px]">
-							<button className="custom-prev-topic flex h-[40.42px] w-[40.42px] items-center justify-center rounded-full border-[0.92px] border-[#9B9B9B]">
-								<Image
-									src={'/assets/icons/categories/Arrows/left-arrow.svg'}
-									alt="right arrow"
-									width={8.13}
-									height={13.71}
-								/>
-							</button>
-							<button className="h-[45.4px] w-[135.05px] rounded-[89.66px] bg-[#E9082A] text-[20.18px] font-[400] leading-[34.41px] text-white hover:bg-black hover:text-white">
-								View more
-							</button>
-							<button className="custom-next-topic flex h-[40.42px] w-[40.42px] items-center justify-center rounded-full border-[0.92px] border-[#9B9B9B]">
-								<Image
-									src={'/assets/icons/categories/Arrows/right-arrow.svg'}
-									alt="right arrow"
-									width={8.13}
-									height={13.71}
-								/>
-							</button>
-						</div>
-					</Swiper>
+				</div>
+				{/* add button and custom arrow for slide*/}
+				<div className="mt-[58.53px] flex w-full items-center justify-center gap-[14.7px]">
+					<button className="h-[45.4px] w-[135.05px] rounded-[89.66px] bg-[#E9082A] text-[20.18px] font-[400] leading-[34.41px] text-white hover:bg-black hover:text-white">
+						View more
+					</button>
 				</div>
 			</Wrapper>
 		</section>
